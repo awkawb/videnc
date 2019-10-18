4c8488254dc4e8234201b8ada47e4b00270ebd05
Andrew Bryant <abryant288@gmail.com>
2019-10-17 22:10:49 -0400

    Added python-find to install_requires.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/awkawb/videnc',
-    install_requires='av',
+    install_requires=['av','python-find'],
     packages=setuptools.find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
